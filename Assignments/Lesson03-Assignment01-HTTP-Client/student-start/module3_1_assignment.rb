9196e23ae86e317c793f5f734535d730c565a828
Alejandro Guevara <alexg@auctionedge.com>
2018-11-01 17:06:53 -0600

    Add sample API call

--- Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,19 @@
 require 'httparty'
 
 class Recipe
+  include HTTParty
+  APIKey = "1d700c0306fe2a800ec045a49285c8b1"
+  base_uri "www.food2fork.com"
 
+  def initialize
+    @options = { query: { key: APIKey, q: 'chicken breast' } }
+  end
+
+  def for(keyword)
+    
+    self.class.get("/api/search", @options)
+  end
 end
+
+recipes = Recipe.new
+puts recipes.for("chicken")
